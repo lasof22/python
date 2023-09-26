3776231fa4a18bb741924dfb3a36baae974844dc
laura <lsralaura.romero@gmail.com>
2023-09-26 07:49:59 -0500

    added REPL topic

---
+++ introduction/calculator_REPL.py
@@ -0,0 +1,16 @@
+#Operators
+
+# +	Addition	2 + 2
+# –	Subtraction	3 – 1
+# *	Multiplication	5 * 3
+# /	Division	5 / 2
+# %	 Modulo	5 % 2 (is the remainder when dividing for example when you divide 5 by 3 the modulo is 2)
+# //	Floor division	9 // 2 (a division operation that rounds the result down to the nearest whole number or integer, which is less than or equal to the normal division result.)
+# **	Exponential	2 ** 4
+
+#Trick
+#You can obtain the result of the last expression in a Python REPL with the underscore operator,
+#>>> 3 * 3
+# 9
+# >>> _ + 3
+# 12
---
+++ introduction/exploring_REPL.py
@@ -0,0 +1,11 @@
+# REPL = Read–Eval–Print Loop
+# Open REPL = Write in a terminal python
+# Close REPL = Write exit() command
+
+# Example
+# PS C:\Users\Vanessa Quintin\Desktop\Learning\Python> python
+# Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> print("Hello world")
+# Hello world
+# >>> exit()
