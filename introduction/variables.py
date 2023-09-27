f8a899f314f6d1471e9356ee6455b0c4d80632b5
laura <lsralaura.romero@gmail.com>
2023-09-27 10:28:17 -0500

    added variables and string(1) topics

---
+++ introduction/variables.py
@@ -0,0 +1,36 @@
+# To define a variable we have to use "="
+# result = 15
+# The type()  function is use to determine a variable or expression type
+#Example 1
+# >>> type(my_nyumber) 
+# <class 'int'>
+#Example 2
+# >>> my_string='Hello'  
+# >>> type(my_string)
+# <class 'str'>
+
+#Variable names aknowledge
+# Lowercase and uppercase letters: a-z and A-Z
+# Numbers: 0-9
+# Underscores: _
+# Variable names must start with a letter or the underscore character and can not start with a number
+#Names are case-sensitive
+
+#Examples
+# Here are some valid variable names:
+
+# name_1,
+# name_2,
+# _database_connection
+# These are invalid names:
+
+# 1tomany (don’t start with a number)
+# my-number (- is not allowed)
+# my number (spaces are not allowed)
+# And these variables are not the same due to case sensitivity:
+
+# cartTotal
+# carttotal
+
+#Note: Use camel case for called classes 
+#Preferly don't use camel case for called variables
